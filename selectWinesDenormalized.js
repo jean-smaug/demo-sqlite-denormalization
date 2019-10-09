daabe29c0c8bac35c5d6a000c2ce2ca7a36d30a8
maxime blanc <maxime.blanc@deindeal.ch>
2019-10-09 13:11:50 +0200

    inverted query

---
+++ selectWinesDenormalized.js
@@ -0,0 +1,29 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("db.sqlite");
+const { performance } = require('perf_hooks');
+
+let start = performance.now()
+
+// SELECT DISTINCT wines.id, wines.name
+// FROM wines
+// WHERE id IN (
+//     SELECT DISTINCT wines_ids
+//     FROM bars_denormalized
+//     WHERE country = 'spain'
+// );
+
+
+db.all(`
+    SELECT DISTINCT json_each.value AS id
+    FROM bars_denormalized, json_each(bars_denormalized.wines_ids)
+    WHERE bars_denormalized.country = 'Kuwait';
+    `,
+    (err, bars) => {
+        if(err) console.error(err)
+
+        console.log(bars)
+
+        let end = performance.now()
+        console.log(end - start)
+    }
+)
