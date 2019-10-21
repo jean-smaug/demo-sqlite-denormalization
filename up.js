e950e2925065cee3bdfa378ccbe13786047eddbf
maxime blanc <maxime.blanc@deindeal.ch>
2019-10-21 12:17:11 +0200

    use better-sqlite

--- up.js
+++ up.js
@@ -1,5 +1,5 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("db.sqlite");
+const Sqlite = require("better-sqlite3");
+const db = new Sqlite("db.sqlite");
 const faker = require("faker");
 
 const NUMBER_OF_BARS = 1000;
@@ -22,110 +22,105 @@ function getRandom(arr, n) {
 
 const shouldDrop = process.env.DROP === "true";
 
-db.serialize(() => {
-  if (shouldDrop) db.run("DROP TABLE IF EXISTS wines");
-  db.run(`
-        CREATE TABLE IF NOT EXISTS wines (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            country TEXT,
-            year NUMBER
-        );
-    `);
+if (shouldDrop) db.prepare("DROP TABLE IF EXISTS wines").run();
+db.prepare(`
+      CREATE TABLE IF NOT EXISTS wines (
+          id TEXT PRIMARY KEY,
+          name TEXT,
+          country TEXT,
+          year NUMBER
+      );
+  `).run();
 
-  Array(NUMBER_OF_WINES)
-    .fill()
-    .forEach(() => {
-      db.run(
+Array(NUMBER_OF_WINES)
+  .fill()
+  .forEach(() => {
+    db.prepare(
+      `
+          INSERT INTO wines (id, name, country, year) 
+          VALUES ($id, $name, $country, $year)
+      `,
+    ).run({
+      id: faker.random.uuid(),
+      name: faker.lorem.words(),
+      country: faker.address.country(),
+      year: faker.date.past(50).getFullYear()
+    });
+  });
+
+if (shouldDrop) db.run("DROP TABLE IF EXISTS bars_denormalized");
+db.prepare(`
+      CREATE TABLE IF NOT EXISTS bars_denormalized (
+          id TEXT PRIMARY KEY,
+          name TEXT,
+          country TEXT,
+          wines_ids JSON
+      );
+  `).run();
+
+if (shouldDrop) db.prepare("DROP TABLE IF EXISTS bars_normalized").run();
+db.prepare(`
+      CREATE TABLE IF NOT EXISTS bars_normalized (
+          id TEXT PRIMARY KEY,
+          name TEXT,
+          country TEXT
+      );
+  `).run();
+
+if (shouldDrop) db.prepare("DROP TABLE IF EXISTS bars_wines").run();
+db.prepare(`
+      CREATE TABLE IF NOT EXISTS bars_wines (
+          bar_id TEXT,
+          wine_id TEXT
+      );
+  `).run();
+
+const rows = db.prepare("SELECT DISTINCT id FROM wines").all()
+const ids = rows.map(item => item.id);
+Array(NUMBER_OF_BARS)
+  .fill()
+  .forEach(wine => {
+    const bar = {
+      id: faker.random.uuid(),
+      name: faker.lorem.words(),
+      country: faker.address.country()
+    };
+
+    const winesIds = getRandom(
+      ids,
+      20
+    );
+
+    db.prepare(
+      `
+            INSERT INTO bars_denormalized (id, name, country, wines_ids) 
+            VALUES ($id, $name, $country, json($winesIds))
         `
-            INSERT INTO wines (id, name, country, year) 
-            VALUES ($id, $name, $country, $year)
+    ).run({
+      ...bar,
+      winesIds: JSON.stringify(winesIds)
+    });
+
+    db.prepare(
+      `
+            INSERT INTO bars_normalized (id, name, country) 
+            VALUES ($id, $name, $country)
         `,
+      {
+        ...bar
+      }
+    );
+
+    winesIds.forEach(wineId => {
+      db.prepare(
+        `
+                INSERT INTO bars_wines (bar_id, wine_id) 
+                VALUES ($barId, $wineId)
+            `,
         {
-          $id: faker.random.uuid(),
-          $name: faker.lorem.words(),
-          $country: faker.address.country(),
-          $year: faker.date.past(50).getFullYear()
+          barId: bar.$id,
+          wineId: wineId
         }
       );
     });
-
-  if (shouldDrop) db.run("DROP TABLE IF EXISTS bars_denormalized");
-  db.run(`
-        CREATE TABLE IF NOT EXISTS bars_denormalized (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            country TEXT,
-            wines_ids JSON
-        );
-    `);
-
-  if (shouldDrop) db.run("DROP TABLE IF EXISTS bars_normalized");
-  db.run(`
-        CREATE TABLE IF NOT EXISTS bars_normalized (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            country TEXT
-        );
-    `);
-
-  if (shouldDrop) db.run("DROP TABLE IF EXISTS bars_wines");
-  db.run(`
-        CREATE TABLE IF NOT EXISTS bars_wines (
-            bar_id TEXT,
-            wine_id TEXT
-        );
-    `);
-
-  db.all("SELECT DISTINCT id FROM wines", (err, rows) => {
-    const ids = rows.map(item => item.id);
-    Array(NUMBER_OF_BARS)
-      .fill()
-      .forEach(wine => {
-        const bar = {
-          $id: faker.random.uuid(),
-          $name: faker.lorem.words(),
-          $country: faker.address.country()
-        };
-
-        const winesIds = getRandom(
-          ids,
-          20
-        );
-
-        db.run(
-          `
-                INSERT INTO bars_denormalized (id, name, country, wines_ids) 
-                VALUES ($id, $name, $country, json($winesIds))
-            `,
-          {
-            ...bar,
-            $winesIds: JSON.stringify(winesIds)
-          }
-        );
-
-        db.run(
-          `
-                INSERT INTO bars_normalized (id, name, country) 
-                VALUES ($id, $name, $country)
-            `,
-          {
-            ...bar
-          }
-        );
-
-        winesIds.forEach(wineId => {
-          db.run(
-            `
-                    INSERT INTO bars_wines (bar_id, wine_id) 
-                    VALUES ($barId, $wineId)
-                `,
-            {
-              $barId: bar.$id,
-              $wineId: wineId
-            }
-          );
-        });
-      });
   });
-});
